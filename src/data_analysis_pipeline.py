d064d6d46aef14997aa1bc3b4e7aa5849e8c8d24
Artem Kurdikov <kourtema@gmail.com>
2025-12-28 20:30:04 +0300

    feat: add data analysis pipeline script

---
+++ src/data_analysis_pipeline.py
@@ -0,0 +1,46 @@
+import os
+import sys
+from cli import cli
+
+if __name__ == "__main__":
+    # Тестовые данные
+    for file in ["Из статьи.json", "Контрпример.json", "Магистерский пример.json"]:
+        print(f"Processing file: {file}")
+        sys.argv = [
+            "data_analysis_pipeline.py",
+            "analyze",
+            "-m",
+            "EABM",
+            "-b",
+            "local",
+            "-f",
+            file
+        ]
+        try:
+            cli()
+        except SystemExit:
+            pass
+    
+    
+    # Синетические данные
+    for file in [
+        file for file in os.listdir("./synthetic_data") if file.endswith(".json")
+    ]:
+        print(f"Processing file: {file}")
+        sys.argv = [
+            "data_analysis_pipeline.py",
+            "analyze",
+            "-m",
+            "EABM",
+            "-b",
+            "local",
+            "-f",
+            f"./synthetic_data/{file}",
+            "-o",
+            "./synthetic_data/results/"
+        ]
+        try:
+            cli()
+        except SystemExit:
+            pass
+    print("All files processed")
